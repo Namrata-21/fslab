da80053ecf4396af9c10a1a92c41fb370efcf0b5
1CR13IS049 <1CR13IS049@iseadmin-VirtualBox>
2016-02-22 14:22:34 +0530

    commited

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -6,10 +6,76 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
+//============================================================================
+// Name        : prg2.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+#include <iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+class student
+	{
+private:string name;
+        string usn;
+        string branch;
+        string sem;
+
+public:void pack();
+       void read();
+	   void search();
+	   void del();
+	   void modifiy();
+	   void unpack();
+};
+fstream f1;
+int main()
+{
+	int choice;
+	student s;
+
+			while (1)
+
+			{
+				cout<<"select the choice 1.insert 2.search 3.del 4.modifiy";
+											cin>>choice;
+			switch(choice)
+			{
+			case 1:s.read();
+			        s.pack();
+			break;
+			//case 2:search();
+				//	break;
+			//case 3:del();
+				//	break;
+			//case 4:modifiy();
+					//break;
+			}
+			}
 }
+void student::read()
+{
+				cout<<"enter name"<<endl;
+				cin>>name;
+				cout<<"enter  usn"<<endl;
+				cin>>usn;
+				cout<<"enter branch"<<endl;
+				cin>>branch;
+				cout<<"enter sem"<<endl;
+				cin>>sem;
+}
+void student::pack()
+{
+	string temp,i;
+	temp=usn+'|'+name+'|'+branch+'|'+sem;
+	temp.resize(100,'$');
+	f1.open("data.txt",ios::out|ios::app);
+	f1<<temp<<endl;
+	f1.close();
+}
+
+
